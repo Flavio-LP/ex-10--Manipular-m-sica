3e82def225ffa9b385cab7f74b43b7d24f3a42b0
Flávio <flavio5070@outlook.com>
2021-06-07 12:45:20 -0300

    versão funcional

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app=express()
 const routes = require('./api/routes')
+const PORT = process.env.PORT || 8080
 
 app.use('/api',routes)
 
-app.listen(8080,function(){
+app.listen(PORT,function(){
     console.log("Servidor online")
 })
 
