8c26dd8e083ddac66b42e9bd8457a2260973c86e
Flávio <flavio5070@outlook.com>
2021-06-07 12:51:29 -0300

    rota raiz

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express()
 const routes = require('./api/routes')
 const PORT = process.env.PORT || 8080
 
+app.get('/',function(req,res){
+    res.send("por favor insira /listar para ver o conteúdo da API")
+})
+
 app.use('/api',routes)
 
 app.listen(PORT,function(){
